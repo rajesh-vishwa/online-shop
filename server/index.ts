45c918dd63ec0b3aa4e1563c235c3351baa32de7
RV <sms.rajesh@gmail.com>
2023-06-20 17:23:05 +0530

    first rout

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,9 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.listen(8000, () => console.log('Server running'))
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(8000, () => console.log("Server running"));
