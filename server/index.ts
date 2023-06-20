d6d5a33da08f92ea213ed415a3c09211da45ff6e
RV <sms.rajesh@gmail.com>
2023-06-20 19:01:04 +0530

    package.json added

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import compress from "compression";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(compress());
+// secure apps by setting various HTTP headers
+app.use(helmet());
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
